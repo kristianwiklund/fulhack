4f9775895c86ce5e168d48f4dd8a5b79c8d1b494
Kristian Wiklund <githubkristian@snabela.nl>
2017-09-01 17:36:39 +0000

    x

---
+++ build-telldus.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+
+apt-get update -q
+apt-get --assume-yes install git build-essential cmake doxygen libftdi-dev  libconfuse-dev
+git clone https://github.com/telldus/telldus
+cd telldus/telldus-core
+mkdir -p build
+cd build
+cmake -DFORCE_COMPILE_FROM_TRUNK=ON ..
+make
+make install
+cat << EOM > /lib/systemd/system/telldusd.service
+# service description from http://andreasahrens.se/getting-telldus-core-to-run-on-raspian-jessie-raspberry-pi/
+[Unit]
+Description=Tellstick service daemon
+After=multi-user.target
+
+[Service]
+Type=forking
+ExecStart=/usr/local/sbin/telldusd
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+systemctl daemon-reload
+systemctl enable telldusd.service
+systemctl start telldusd.service
+systemctl status telldusd.service
+
+
