411ab12fcc52648705a0317da1fc1c66e8f1e899
Kristian Wiklund <githubkristian@snabela.nl>
2017-09-09 16:45:38 +0000

    added bsd license

--- build-telldus.sh
+++ build-telldus.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#Copyright (c) 2017 <copyright holder>.
+#All rights reserved.
+
+#Redistribution and use in source and binary forms are permitted
+#provided that the above copyright notice and this paragraph are
+#duplicated in all such forms and that any documentation,
+#advertising materials, and other materials related to such
+#distribution and use acknowledge that the software was developed
+#by the <organization>. The name of the
+#<organization> may not be used to endorse or promote products derived
+#from this software without specific prior written permission.
+#THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+#IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 echo "Building telldusd from github with new libraries"
 echo "Installing dependencies..."
 
